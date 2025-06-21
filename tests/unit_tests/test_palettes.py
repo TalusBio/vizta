440b57b775e96652f341748a853a405b5293fab6
William Fondrie <fondriew@gmail.com>
2025-06-20 21:05:01 -0700

    fix tests again

--- tests/unit_tests/test_palettes.py
+++ tests/unit_tests/test_palettes.py
@@ -3,7 +3,17 @@
 
 def test_vizta_first():
     """Test when importing vizta before seaborn"""
+    import vizta  # noqa: F401
     import seaborn as sns
+
+    sns.color_palette("talusbio")
+    sns.color_palette("wfondrie")
+
+
+def test_vizta_second():
+    """Test when importing vizta after seaborn"""
+    import seaborn as sns
+    import vizta  # noqa: F401
 
     sns.color_palette("talusbio")
     sns.color_palette("wfondrie")
